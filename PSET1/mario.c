3371c303cc594d76744d165edfddfaa2476df3bf
jmastrom <jmastrom@gmail.com>
2014-09-06 09:37:49 -0400

    first commit

---
+++ PSET1/mario.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+
+{
+    printf("Give me a pyramid height betweeen 1-23: ");
+    
+    int n = GetInt();
+    
+    if (n > 0 && n <= 23)
+        
+        for (int i = 0; i < n; i++);
+         //print spaces
+         //print hashes
+         char *h = "#";
+         //print new line
+         printf("%touch README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/jmastrom/cs50x.git
+git push -u origin masters*%n\n", );
+            
+}
+//   char *s = "MyText";
+    unsigned int sz = 9;
+    char *pad = "########################################";
+    printf ("%.*s%s\n", (sz < strlen(s)) ? 0 : sz - strlen(s), pad, s);
