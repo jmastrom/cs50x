87bee61df4a90850890e5675337966f42f02a418
jmastrom <jmastrom@gmail.com>
2014-09-09 21:20:25 -0400

    PSET1 Final Submission

--- PSET1/greedy.c
+++ PSET1/greedy.c
@@ -1,3 +1,10 @@
+/* Greedy.c
+   CS50x PSET1
+   Author: Jimmy Mastrom
+   
+*/
+
+
 #include <stdio.h>
 #include <cs50.h>
 #include <math.h>
@@ -8,7 +15,7 @@ int main(void)
     //initialize change variable
     float change_input;
     int change_dollar;
-    int coin_count;
+    int coin_count = 0;
     int quarter = 25;
     int dime = 10;
     int nickel = 5;
@@ -25,34 +32,42 @@ int main(void)
         }
     } while(change_input < 0);
     
+    change_dollar = (int)round(change_input * 100);
+    
+    
     //convert change_input to int, then covert to dollars by multiplying by 100
     //round()
-
-    //Quarters
-    do {
-       change_dollar-quarter
-       coin_count++;
-       } while (change_dollar > 25);
-        
-    //Dimes
-    do {
-        change_dollar-dime
-        coin_count++;
-        } while (change_dollar > 10);
+    do 
+    {
+            //Quarters
+            while (change_dollar >= 25)
+            { change_dollar = (change_dollar - quarter);
+              coin_count += 1; 
+            }
+            
+            //Dimes
+            while (change_dollar >= 10)
+            { change_dollar = (change_dollar - dime);
+              coin_count += 1;
+            } 
    
-    //Nickels
-    do {
-        change_dollar-dime
-        coin_count++;
-        } while (change_dollar > 5);
+            //Nickels
+            while (change_dollar >= 5)
+            { change_dollar = (change_dollar - nickel);
+              coin_count += 1;
+            } 
         
-    //Pennies
-    do {
-        change_dollar-penny
-        coin_count++
-        } while (change_dollar > 1);
+            //Pennies
+            while (change_dollar >= 1)
+            { change_dollar = (change_dollar - penny);
+              coin_count += 1;
+            }
+      } 
+      while (change_dollar > 0);
         
-    printf(coin_count);
+    printf("%d\n", coin_count); 
+    
+    return 0;
 
 }
 
