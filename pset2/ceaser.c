dfbe1c8e5e4b8441db29a004e14c1f9fa95a7e57
jmastrom <jmastrom@gmail.com>
2014-09-23 21:19:45 -0400

    file attempt

---
+++ pset2/ceaser.c
@@ -0,0 +1,22 @@
+/*ceasar.c is an implementaiton of a simple ceasar cypher
+  it accepts 1 command line argument of 'secret key' of k
+  as the number of 'characters' to shift during the 
+  cypher. 
+  Author: Jimmy Mastrom
+  CS50x
+*/ 
+
+#include <stdio.h>
+#include <cs50.h>
+
+int main(int argc, argv[])
+{
+
+    string k = argv[1];
+    
+    do 
+    { 
+        printf("too many command line arguments!')
+    }
+    while (argc >= 3); 
+}
